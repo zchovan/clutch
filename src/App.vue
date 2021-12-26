0f699cf2f64260c5d2fd8245283f19c9ffd6fd5f
Zoltan Chovan <nezuvian@gmail.com>
2021-12-26 21:06:49 +0100

    dunno

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
       <svg class="m-2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-car-clutch" width="52" height="52" viewBox="0 0 24 24">
         <path d="M10 18.84L14 20.7V23L8 20V14H5V10H8V4L14 1V3.3L10 5.16V18.84M19 10H15V5.41L12 6.8V17.2L15 18.6V14H19V10Z" />
       </svg>
-      <!-- <img src="./assets/clutch_logo.svg" class="w-10 h-10" alt=""/> -->
       <div class="rotate-90 ml-8">
         <span class="text-secondary">Clutch</span>
       </div>
@@ -22,17 +21,19 @@
           <a href="#" class="block py-1 px-2 text-left">Waiting (0)</a>
         </nav>
       </div>
-    <!-- main part -->
-    <div class="w-fit">
-      <div class="main h-1/2">
+    <div class="flex-none">
+      <div>
+        <toolbar/>
+        <torrent-list/>
+      </div>
+      <!-- <div class="main h-1/2">
         <toolbar/>
         <torrent-list/>
       </div> 
-      
-      <!-- bottom part -->
+    
       <div class="bottom h-1/2">
         <h1>bottom</h1>
-      </div>
+      </div> -->
     </div>
   </div>
   
