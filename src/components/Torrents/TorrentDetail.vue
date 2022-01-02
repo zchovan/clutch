4ebfd4f646c76f2bb31f1d4641888a6b17b43be4
Zoltan Chovan <nezuvian@gmail.com>
2022-01-02 21:33:29 +0100

    deleting, adding missing files

---
+++ src/components/Torrents/TorrentDetail.vue
@@ -0,0 +1,47 @@
+<template>  
+  <div class="flex-1 bg-primary-lighter text-gray-light">    
+    <table class="table-auto">
+      <thead>
+        <tr>
+          <th>Song</th>
+          <th>Artist</th>
+          <th>Year</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+          <td>Malcolm Lockyer</td>
+          <td>1961</td>
+        </tr>
+        <tr>
+          <td>Witchy Woman</td>
+          <td>The Eagles</td>
+          <td>1972</td>
+        </tr>
+        <tr>
+          <td>Shining Star</td>
+          <td>Earth, Wind, and Fire</td>
+          <td>1975</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>        
+</template>
+
+<script>
+
+
+export default {
+  name: 'TorrentDetail',
+  components: {        
+  },
+  props: { 
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
